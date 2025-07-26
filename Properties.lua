37165723b2a047ce816edcf074216e5b58f8ab4b
Cesare <79593536+Cesare0328@users.noreply.github.com>
2025-07-26 11:08:22 +0300

    Update Properties.lua

--- Properties.lua
+++ Properties.lua
@@ -163,14 +163,6 @@ local function getRbxApi()
 			}
 		}
 		local gottenprops = getpropertylist(classInstance)
-		for _, prop in ipairs(Properties) do
-		for i, name in ipairs(gottenprops) do
-        		if name == prop.Name then
-            		--table.remove(gottenprops, i)
-            		break
-        		end
-    		end
-		end
 		for _, Property in ipairs(gottenprops) do
     		local Tags = {}
     		local Success, Value = pcall(gethiddenproperty, classInstance, Property)
@@ -199,7 +191,7 @@ local function getRbxApi()
 				end
 			end
 		end
-		sortAlphabetic(Properties, "Name")
+		--sortAlphabetic(Properties, "Name")
 		return Properties
 	end
 
