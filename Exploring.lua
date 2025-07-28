e9108909078eca2c9e6ec53a818e4dc1b0506bf1
Cesare <79593536+Cesare0328@users.noreply.github.com>
2025-07-28 17:32:17 +0300

    Update Exploring.lua

--- Exploring.lua
+++ Exploring.lua
@@ -1942,6 +1942,8 @@ function rightClickMenu(sObj)
 		'Copy Path'
 	}
 	local IsSearching = explorerFilter.Text ~= "" and (explorerFilter.Text == "Filter Instances" and Searched)
+	rconsoleprint(IsSearching)
+	rconsoleprint(explorerFilter.Text)
 	if sObj == RunningScriptsStorageMain or sObj == NilStorageMain then
 		table_insert(actions, 1, "Refresh Instances")
 	elseif IsA(sObj, "RemoteEvent") or IsA(sObj, "RemoteFunction") then
