79d33d54121f2c9673c8021e3a2b330194335917
Cesare <79593536+Cesare0328@users.noreply.github.com>
2025-07-29 09:42:55 -0700

    Update Exploring.lua

--- Exploring.lua
+++ Exploring.lua
@@ -1947,8 +1947,8 @@ function rightClickMenu(sObj)
 		table_insert(actions, 10, "Call Remote")
 	elseif IsA(sObj, "BasePart") or IsA(sObj, "Model") or IsA(sObj, "Humanoid") or IsA(sObj, "Player") then
 		table_insert(actions, 8, "Teleport to")
-	elseif explorerFilter.Text ~= "" and (explorerFilter.Text ~= "Filter Instances" and Searched) then
-		table_insert(actions, 8, "Clear Search and Jump to")
+	elseif explorerFilter.Text ~= "" and (explorerFilter.Text == "Filter Instances" and Searched) then
+		table_insert(actions, 1, "Clear Search and Jump to")
 	elseif IsA(sObj, "ClickDetector") then
 		table_insert(actions, 8, "Fire ClickDetector")
 	elseif IsA(sObj, "TouchTransmitter") then
@@ -3108,7 +3108,7 @@ Connect(UserInputService.InputBegan, function(p1)
 		HoldingCtrl = true
 	end
 	if p1.UserInputType == Enum.UserInputType.MouseButton1 then
-        DestroyRightClick()
+        --DestroyRightClick()
     end
 end)
 
