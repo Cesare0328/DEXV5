2a2a40c6529efc24ead766b1813c9e673f7b82a8
Cesare <79593536+Cesare0328@users.noreply.github.com>
2025-08-03 18:42:08 -0700

    Update Specials.lua

--- Specials.lua
+++ Specials.lua
@@ -444,9 +444,7 @@ end
 -- < Source > --
 local Specials = {
 	checkrbxlocked = function(p1)
-		warn(p1:GetFullName())
-		if not p1:IsDescendantOf(game) then p1 = NilInstances[p1] print(NilInstances[p1]:GetFullName()) end
-		warn(p1:GetFullName())
+		if not p1:IsDescendantOf(game) then p1 = NilInstances[p1] end
     	local success, err = pcall(function()
         	p1.Parent = p1
     	end)
