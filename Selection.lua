35c7dab23614e474a60dae74bd0e79f5e070ef3d
Cesare <79593536+Cesare0328@users.noreply.github.com>
2025-08-12 20:44:16 +0200

    Update Selection.lua

--- Selection.lua
+++ Selection.lua
@@ -705,7 +705,7 @@ end
 
 local function SerializeInstance(instance, output, saveScripts, avoidPlayerCharacters, saveNilInstances, processed, total, statusCallback)
     if SaveMapSettings.ProgressiveSave then task.wait(0) end
-    if instance.ClassName:find("Wrap") then warn(instance.Name) return processed end
+    if instance.ClassName:find("Wrap") then return processed end
     if Blacklist[instance.ClassName] or Blacklist[instance.Name] then
         statusCallback(processed, total, "Skipping blacklisted instance: " .. (instance:GetFullName() or "Unnamed"))
         return processed
