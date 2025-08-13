a41ba704b12b6385fd14777c7b55637ea557b618
Cesare <79593536+Cesare0328@users.noreply.github.com>
2025-08-13 10:09:24 +0200

    Update Exploring.lua

--- Exploring.lua
+++ Exploring.lua
@@ -1112,7 +1112,7 @@ local explorerFilter =  Create('TextBox',{
 })
 
 local iconBox1 = Create('ImageButton', {
-    Image = "rbxassetid://100655995813182", 
+    Image = "rbxassetid://90870016760973", 
     BackgroundColor3 = Color3.new(1, 1, 1),
     BackgroundTransparency = 1,
     AnchorPoint = Vector2.new(1, 0.5),
@@ -1123,7 +1123,7 @@ local iconBox1 = Create('ImageButton', {
 iconBox1.Parent = explorerFilter
 
 local iconBox2 = Create('ImageButton', {
-    Image = "rbxassetid://107359618045188", 
+    Image = "rbxassetid://86281108620503", 
     BackgroundColor3 = Color3.new(1, 1, 1),
     BackgroundTransparency = 1,
     AnchorPoint = Vector2.new(1, 0.5),
