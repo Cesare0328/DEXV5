b7eddf657b16bdc1d01678db4f9f6940c99d6f36
Cesare <79593536+Cesare0328@users.noreply.github.com>
2025-08-17 05:19:04 +0200

    Update Properties.lua

--- Properties.lua
+++ Properties.lua
@@ -72,7 +72,7 @@ local function splitStringIntoChunks(jsonStr)
 end
 
 local function DexCacheAssets()
-local Assets = {"Loading.png", "Finished.png", "icon_regions_copy.png", "icon_regions_paste.png", "icon_regions_delete.png", "star_filled.png", "star_stroke.png", "btn_expand.png", "btn_collapse.png"}
+local Assets = {"Loading.png", "Finished.png", "icon_regions_copy.png", "icon_regions_paste.png", "icon_regions_delete.png", "star_filled.png", "star_stroke.png", "btn_expand.png", "btn_collapse.png", "tag.png", "locked.png", "anchored.png", "transparency.png", "material.png", "mesh.png", "texture.png"}
 if not isfolder("DEXV5") then makefolder("DEXV5") end
 if not isfolder("DEXV5\\Assets") then makefolder("DEXV5\\Assets") end
 for i,v in pairs(Assets) do
