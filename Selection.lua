efdd5923546fb660ef1647cfde9b0b1ad2c3f460
Cesare <79593536+Cesare0328@users.noreply.github.com>
2025-08-26 17:53:34 +0200

    Update Selection.lua

--- Selection.lua
+++ Selection.lua
@@ -942,7 +942,7 @@ local function SerializeInstance(instance, output, saveScripts, avoidPlayerChara
                     end
                 end
                 if not passed then
-                    local success, sourceAssetId = tonumber(gethiddenproperty(instance, "SourceAssetId"))
+                    local sourceAssetId, success = tonumber(gethiddenproperty(instance, "SourceAssetId"))
                     if success and sourceAssetId and sourceAssetId ~= -1 then
                         local asset = LoadLocalAsset(InsertService, "rbxassetid://" .. sourceAssetId)
                         if asset then
