6a78cffaf6839e5802ec05d313a1bb4bb4aee7b1
Cesare <79593536+Cesare0328@users.noreply.github.com>
2025-09-02 15:29:26 +0200

    Update Exploring.lua

--- Exploring.lua
+++ Exploring.lua
@@ -2122,6 +2122,7 @@ do
 	end
 
 	SetSelection_Bindable.OnInvoke = function(...)
+		warn(#CoreGui:GetGuiObjectsAtPosition(Mouse.X, Mouse.Y))
 		if #CoreGui:GetGuiObjectsAtPosition(Mouse.X, Mouse.Y) > 0 then return end
 		SetSelectionBox2D(nil)
 		Selection:Set(...)
