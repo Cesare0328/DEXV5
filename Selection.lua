d3fccfb4483cc1df197e2b26a55525b34ee282c5
Cesare <79593536+Cesare0328@users.noreply.github.com>
2025-12-27 14:01:08 +0100

    Update Selection.lua

--- Selection.lua
+++ Selection.lua
@@ -739,7 +739,7 @@ local function PromptStreamingEnabledCaution(TitleLabel)
 end
 
 local function SerializeInstance(instance, output, saveScripts, avoidPlayerCharacters, saveNilInstances, processed, total, statusCallback)
-    if SaveMapSettings.ProgressiveSave and not instance == workspace.CurrentCamera then task.wait(0) end
+    if SaveMapSettings.ProgressiveSave and not instance == workspace.CurrentCamera then task.wait(0.05) end
     if instance.ClassName:find("Wrap") then return processed end
     if instance:IsA("Bone") and (not instance.Parent or not instance.Parent:IsA("BasePart") or instance.Parent:IsA("Bone")) then return processed end
     if Blacklist[instance.ClassName] or Blacklist[instance.Name] then
